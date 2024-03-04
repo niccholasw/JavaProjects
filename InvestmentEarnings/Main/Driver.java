fa15e6c64d488fa4518de70549fcbe7945708dcf
nwil850 <nwil850@aucklanduni.ac.nz>
2024-03-04 14:11:49 +1300

    Changed numer formatting to use currency format instead of just decimal

--- InvestmentEarnings/Main/Driver.java
+++ InvestmentEarnings/Main/Driver.java
@@ -1,5 +1,5 @@
 import java.util.Scanner; //import Scanner class from java.util package
-import java.text.*; //import DecimalFormat class from java.text package
+import java.text.NumberFormat; //import NumberFormat class from java.text package
 
 public class Driver {
     public static void main(String[] args) { //main method
@@ -8,7 +8,7 @@ public class Driver {
         double buyingPrice = scanner.nextDouble(); //get buying price from user
         int day = 1; //initialize day to 1
         double closingPrice = buyingPrice; //initialize closing price to buying price
-        DecimalFormat df = new DecimalFormat("0.00"); //create DecimalFormat object
+        NumberFormat nf = NumberFormat.getCurrencyInstance(); //create NumberFormat object
 
         while (true) { //loop until user enters -1
             System.out.println("Enter the closing price for day " + day + " or -1 to exit: "); //prompt user to enter closing price
@@ -16,9 +16,9 @@ public class Driver {
             if (closingPrice == -1) break; //exit loop if user enters -1
             double earnings = closingPrice - buyingPrice; //calculate earnings
             if (earnings > 0) { //if earnings is positive
-                System.out.println("Your stock has increased by " + df.format(earnings) + " per share on day " + day); //print earnings
+                System.out.println("Your stock has increased by " + nf.format(earnings) + " per share on day " + day); //print earnings
             } else if (earnings < 0) { //if earnings is negative
-                System.out.println("You stock has decreased by " + df.format(earnings) + " per share on day " + day); //print loss
+                System.out.println("You stock has decreased by " + nf.format(earnings) + " per share on day " + day); //print loss
             } else { //if earnings is zero
                 System.out.println("You broke even on day " + day); //print break even
             }
@@ -26,4 +26,4 @@ public class Driver {
         }
         scanner.close(); //close scanner
     }
-} 
+}
