43e2e0a53db90347499813e3c45b98c75730fff2
nwil850 <nwil850@aucklanduni.ac.nz>
2024-03-26 22:18:42 +1300

    Learning how to use recursion in java

---
+++ Randomthings/RecursionPractise.java
@@ -0,0 +1,14 @@
+//Learning how to use recursion
+
+//Recursion is a method that calls itself
+
+
+public class RecursionPractise {
+    public static void main(String[] args) {
+        sayHi();
+    }
+
+    private static void sayHi(){
+        System.out.println("Hi");
+    }
+}
